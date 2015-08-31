ce57718c2fdf5fc94913b622bce627f7bd7a66f6
Javier <kientienemibarraespaciadora@gmail.com>
2015-08-31 12:58:43 +0200

    version 1.0

---
+++ SwiftUISearchBar/SwiftUISearchBar/Candy.swift
@@ -0,0 +1,14 @@
+//
+//  Candy.swift
+//  SwiftUISearchBar
+//
+//  Created by Javier on 30/8/15.
+//  Copyright (c) 2015 Javier. All rights reserved.
+//
+
+import Foundation
+
+struct Candy {
+    let category : String
+    let name : String
+}
